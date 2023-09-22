d6acaafb495acac3ddd09193eec3470a333e3ab0
dvbisht <digvijay.bisht85@gmail.com>
2023-09-22 06:56:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "dv@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 111222;
+console.log(accountId);
+/*
+Prefer not to use var
+because of issues in block scope and functional scope
+*/
